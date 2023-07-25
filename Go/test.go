81b3da79c9570c42a477265fa1e717b49d8892e3
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-07-25 16:43:30 +0900

    Create test.go

---
+++ Go/test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var x, y, z int = 1, 2, 3
+var hello string = "Hello"
+
+func main() {
+	fmt.Println("happy", math.Pi, "Day!")
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println(hello)
+}
