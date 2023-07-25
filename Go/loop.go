fe96e9f5aa3206f99d1bc1e442500354bad1dec2
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-07-25 16:47:03 +0900

    Create loop.go

---
+++ Go/loop.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func printhello(x int) (int) {
+for i := 0 ; i < 10 ; i++ {
+  if i%2 == 0 {
+    fmt.Println("Hello World")  
+  }
+  }
+  return x;
+}
+func main() {
+	printhello(5)
+}
