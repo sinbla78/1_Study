d5d8a00bf92576b7d624a84176ce29edae96b764
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-07-26 09:33:54 +0900

    Create Map.go

---
+++ Go/Map.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	myMap := map[string]string{
+		"A": "Apple",
+		"B": "Banana",
+		"C": "Charlie",
+	}
+	for key, val := range myMap {
+		fmt.Println(key, val)
+	}
+}
